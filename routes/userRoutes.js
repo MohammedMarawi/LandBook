71a03a9b6fe3b64dee6927faf964c404160f6bb4
mohammedMarawi <mohammedzzzzaaaazzzzaaaa@gmila.com>
2025-08-22 21:57:03 +0300

    add logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const userControllers = require('../controllers/userController');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.git('/logout' , authController.logout) ; 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
